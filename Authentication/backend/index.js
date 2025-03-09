b60949a98c4b21e79ef3bfb3c8a044d53332ac82
jim2107054 <mdjahid205hasan@gmail.com>
2025-03-09 22:55:23 +0600

    creating schema

--- Authentication/backend/index.js
+++ Authentication/backend/index.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 dotenv.config();
 
 const app = express();
 
-let port = process.env.PORT || 8000;//if we don't have port in env file then it will run on 8000
+let port = process.env.PORT || 4000;//if we don't have port in env file then it will run on 4000
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.listen(port, () => {
+  connectDB();
   console.log(`Server is running on port ${port}`);
 });
