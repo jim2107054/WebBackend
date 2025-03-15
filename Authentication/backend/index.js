7a1754d81cf1aa16ec377b2f5422c3ad7593a5a1
jim2107054 <mdjahid205hasan@gmail.com>
2025-03-15 15:13:42 +0600

    finally fix the issue.

--- Authentication/backend/index.js
+++ Authentication/backend/index.js
@@ -7,11 +7,9 @@ dotenv.config();
 let app = express();
 
 let port = process.env.PORT || 4000;//if we don't have port in env file then it will run on 4000
-
+app.use(express.json());
 // Middleware
 app.use("/api/auth",authRouter); // we are using the authRouter from the auth.routes.js file. because we want to use the routes from that file
-
-app.use(express.json());
 
 app.get("/",(req,res)=>{
   res.send("Server is running");
@@ -21,3 +19,4 @@ app.listen(port, () => {
   connectDB();
   console.log(`Server is running on port ${port}`);
 });
+
