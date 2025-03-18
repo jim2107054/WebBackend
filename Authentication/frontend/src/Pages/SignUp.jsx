793f02adb10f052f21f26123424a84d48b595bdd
jim2107054 <mdjahid205hasan@gmail.com>
2025-03-18 12:57:18 +0600

    add img box

--- Authentication/frontend/src/Pages/SignUp.jsx
+++ Authentication/frontend/src/Pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { dataContext } from "../context/UserContext";
 import axios from 'axios';
+import dp from '../assets/dp.webp';
 
 function SignUp() {
   let {serverUrl} = useContext(dataContext);
@@ -22,6 +23,7 @@ function SignUp() {
         password
       },{withCredentials: true});//withCredentials: true is used to send cookies along with the request.
       console.log(data);
+      alert("User Signed Up Successfully");
     }
     catch(error){
       console.log(error.message);
@@ -52,8 +54,17 @@ function SignUp() {
     <div className="bg-[#404b4a] w-full h-[100vh] justify-center items-center flex">
       {/* for signup from */}
       <div className="w-[85%] max-w-[600px] h-[550px] bg-[#09403b] rounded-xl flex flex-col justify-center items-center gap-[15px]">
-        <h1 className="text-white -mt-20 mb-5 text-[20px] font-semibold">Sign Up</h1>
+        <h1 className="text-white -mt-2 mb-5 text-[20px] font-semibold">Sign Up</h1>
         <form className="w-full p-0.5 flex flex-col justify-center items-center" onSubmit={handleSignUp}>
+
+        {/* add user image */}
+        <div className="w-[100px] h-[100px] rounded-full bg-[#e4e0ec] overflow-hidden relative border-2 border-white">
+          <img src={dp} alt="dp" className="w-full h-full object-cover rounded-full"/>
+          <div className="absolute w-[100%] h-[100%] bg-black top-0 opacity-0 hover:opacity-50 cursor-pointer flex justify-center items-center text-white font-bold text-2xl">
+            +
+          </div>
+        </div>
+
           <div className="w-[95%] h-[50px] flex justify-center gap-3 mt-4">
             <input type="text" placeholder="First Name" className="w-[50%] rounded p-4" value={firstName} onChange={setFirstNameHandler}/>
             <input type="text" placeholder="Last Name" className="w-[50%] rounded p-4" value={lastName} onChange={setLastNameHandler}/>
